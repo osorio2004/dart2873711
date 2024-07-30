68238943ebfee6a30f58c5c954b9ac98144da5c7
osorio2004 <osoriojuanandres0@gmail.com>
2024-07-30 11:43:13 -0600

    se agrego contenido de POO ejemplo 05

---
+++ ADSO2873711/POO/ejemploPOO05.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'vehiculo.dart';
+
+void main(List<String> args) {
+  int cantInteracciones = 1;
+  String colorUsuario;
+  int velocidadUsuario;
+  double tamanioUsuario;
+  List<Vehiculo> listaVehiculos = [];
+  //ciclo para llenar tipo vehiculo
+  for (var i = 0; i < cantInteracciones; i++) {
+    print("ingrese el color de el vehiculo ${i+1}");
+    colorUsuario = stdin.readLineSync()!;
+    print("ingrese la velocidad de el vehiculo ${i+1}");
+    velocidadUsuario = int.parse(stdin.readLineSync()!);
+    print("ingrese el tamaño de el vehiculo");
+    tamanioUsuario = double.parse(stdin.readLineSync()!);
+    //se instancia la clase vehiculo y se crea el onjetivo con los valores de el usuario
+    Vehiculo vehiculo_obj = Vehiculo(colorUsuario, velocidadUsuario, tamanioUsuario);
+    //se añade el vehiculo a la lista
+    listaVehiculos.add(vehiculo_obj);
+  }
+  //ciclo para recorrer el elmento(objeto) de la lista y llamar los metodos
+  for (var i = 0; i < cantInteracciones; i++) {
+    print('*'*50);
+    print("***vehiculos ${i+1}***");
+    listaVehiculos[i].avanzar(20);
+    listaVehiculos[i].avanzar(80);
+    listaVehiculos[i].avanzar(100);
+    listaVehiculos[i].reducir(10);
+    listaVehiculos[i].detenerse();
+    print('*'*50);
+    print("ingrese el lugar donde se estacionara el auto");
+    lugar = stdin.readLineSync()!;
+    listaVehiculos[i].estacionar(lugar);
+    print("el auto se estaciono en $lugar");
+  }
+}
--- ADSO2873711/POO/vehiculo.dart
+++ ADSO2873711/POO/vehiculo.dart
@@ -16,9 +16,23 @@ class Vehiculo{
       print("no puede superar el limite de 200 km/h");
     }
   }
+  //metodo reducir velocdad
+  void reducir(int velAvanz){
+    int newVelocidad = this.velocidad - velAvanz;
+    if (newVelocidad < 0){
+      this.velocidad = newVelocidad;
+      newVelocidad = 0;
+      print("el vehiculo disminuye su veocidad a ${this.velocidad}");
+    }
+  }
   //metodo detener
   void detenerse(){
-    velocidad =0;
+    velocidad = 0;
     print("el vehiculo se detiene");
+  }
+  //metdo parquear
+  void estacionar(String lugar){
+    this.velocidad = 0;
+    print("el vehiculo fue estacionado en $lugar");
   }
 }
